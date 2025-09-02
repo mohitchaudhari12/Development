e5bc843246fcf79bbf3ec5d7567ddc423bece061
Mohit Chaudhari <mohitchaudhari@skillminds>
2025-09-02 17:36:31 +0530

    updates

---
+++ Javascript Brushup/script3.js
@@ -0,0 +1,17 @@
+/* //example on promises in JS
+const cart = ["Shirt","Pant","Shoes","Kurta"]
+
+createOrder(cart,function(orderId){
+    proceedToPayment(orderId);
+});
+
+const promise = createOrder(cart)
+
+promise.then(function (orderId){
+    proceedToPayment(orderId);
+});
+ */
+
+const GITHUB_API = "https://api.github.com/users/mohitchaudhari-commits" 
+
+const user = fetch(GITHUB_API);
