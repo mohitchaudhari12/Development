8184738adbb4e599d92300f3acdbe28c2f8e2c44
Mohit Chaudhari <mohitchaudhari@skillminds>
2025-09-03 12:07:58 +0530

    add script3

--- Javascript Brushup/script3.js
+++ Javascript Brushup/script3.js
@@ -12,6 +12,27 @@ promise.then(function (orderId){
 });
  */
 
-const GITHUB_API = "https://api.github.com/users/mohitchaudhari-commits" 
+/* const GITHUB_API = "https://api.github.com/users/mohitchaudhari-commits" 
 
 const user = fetch(GITHUB_API);
+
+console.log(user)
+ */
+
+const cart = ["Shirt","Pant","Kurta","Shoes"]
+
+createOrder(cart,function(orderId){
+    proceedToPayment(orderId , function(paymentInfo){
+        showOrderSummary(paymentInfo,function(){
+            updateWalleteBalance();
+        });
+    });
+});
+
+createOrder(cart)
+.then(function(orderId){
+    proceedToPayment(orderId)
+})
+.then(function(orderId){
+    proceedToPayment(orderId)
+})
