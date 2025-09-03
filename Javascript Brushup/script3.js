64e379c5ce8156d217a0e8a6f046d5a2cdd3b799
Mohit Chaudhari <mohitchaudhari@skillminds>
2025-09-03 16:49:38 +0530

    promise in script3

--- Javascript Brushup/script3.js
+++ Javascript Brushup/script3.js
@@ -19,7 +19,7 @@ const user = fetch(GITHUB_API);
 console.log(user)
  */
 
-const cart = ["Shirt","Pant","Kurta","Shoes"]
+/* const cart = ["Shirt","Pant","Kurta","Shoes"]
 
 createOrder(cart,function(orderId){
     proceedToPayment(orderId , function(paymentInfo){
@@ -31,8 +31,73 @@ createOrder(cart,function(orderId){
 
 createOrder(cart)
 .then(function(orderId){
-    proceedToPayment(orderId)
+    return proceedToPayment(orderId)
 })
-.then(function(orderId){
-    proceedToPayment(orderId)
+.then(function(paymentInfo){
+    return showOrderSummary(paymentInfo)
 })
+.then((paymentInfo) => updateWalleteBalance(paymentInfo)) */
+
+
+/* 
+//Promise using arrow function
+const cart = ["shirt","pant","shoes","kurta"]
+createOrder(cart,function(orderId){
+    proceedToPay(orderId,function(paymentInfo){
+        showrderSummary(paymentInfo,function(){
+            updateWalletBalance();
+        });
+    });
+});
+createOrder(cart)
+.then ((orderId) => proceedToPay(orderId))
+.then ((paymentInfo) => showrderSummary(paymentInfo))
+.then ((paymentInfo) => updateWalletBalance(paymentInfo)); */
+
+/* const cart = ["shirt","pant","kurta","shoes"]
+
+createOrder(cart,function(orderId){
+    proceeedToPay(orderId,function(paymentInfo){
+        showOrderSummary(paymentInfo,function(){
+            updateWalletBalance();
+        })
+    })
+})
+createOrder(cart)
+.then((orderId) => proceeedToPay(orderId))
+.then((paymentInfo) => showOrderSummary(paymentInfo))
+.then((paymentInfo) => updateWalletBalance(paymentInfo)) */
+
+
+const cart = ["shoes","pant","kurta"]
+
+const promise = createOrder(cart); //orderaId
+
+promise.then(function(orderId){
+    console.log(orderId)
+    //proceedToPayment(orderId)
+});
+
+///producer
+function createOrder(cart){
+    const pr = new Promise(function (resolve,reject){
+        //create order
+        //validate cart
+        //orderId
+    function validatCart(cart){
+        return true;
+    }
+        if(!validatCart(cart)){
+            const err = new Error("Cart is not valid")
+            reject(err);
+        }
+        //logic for createOrder
+        const orderId = "123456"
+        if(orderId){
+            setTimeout(function(){
+                resolve(orderId)
+            },5000)
+        }
+    });
+    return pr;
+}
